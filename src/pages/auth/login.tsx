340851c61bdc15406054079ef2efbbb8869cd82d
20KMkim02 <112478541+20KMkim02@users.noreply.github.com>
2022-11-01 18:07:30 +0700

    Background

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,69 +1,76 @@
 import type { NextPage } from "next";
-import { Stack, Button, Grid, Container } from "@mui/material";
+import { Stack, Button, Grid, Container, Paper } from "@mui/material";
 import Image from "next/image";
 import { generateAuthRequest } from "@utils/auth/line";
 import { useRouter } from "next/router";
 
+
+const styles = {
+	paperContainer: {
+		backgroundImage: `url(${"/image/bgForLogin.svg"})`
+
+	}
+};
 const Login: NextPage = () => {
 	const router = useRouter();
 	return (
 		<div>
-			<Container
-				maxWidth="xl"
-				style={{
-					backgroundColor: "#FDE",
-				}}
-			>
-				<Grid
-					container
-					justifyContent="center"
-					alignItems="center"
-					direction="column"
-					style={{ minHeight: "100vh" }}
-				>
-					<h1
-						style={{
-							fontSize: "3rem",
-							fontWeight: "bold",
-							color: "#111827",
-						}}
+			<Container>
+				<Paper style={styles.paperContainer}>
+
+
+					<Grid
+
+						container
+						justifyContent="center"
+						alignItems="center"
+						direction="column"
+						style={{ minHeight: "100vh" }}
 					>
-						Login with Line
-					</h1>
-					<Stack direction="row" spacing={2}>
-						<Button
-							// onClick={async () => {
-							// 	router.push(await generateAuthRequest("login"));
-							// }} //NOmoreLogin
-							variant="contained"
-							color="success"
+						<h1
 							style={{
-								backgroundColor: "#FFFFFF",
-								color: "black",
+								fontSize: "3rem",
 								fontWeight: "bold",
-								fontSize: "1.2rem",
-								borderRadius: "2rem",
-								padding: "0.5rem 1rem",
-								margin: "0.5rem 1rem",
-								border: "none",
-								boxShadow: "0 0 0.5rem rgba(0, 0, 0, 0.5)",
+								color: "#111827",
 							}}
 						>
-							
+							Login with Line
+						</h1>
+						<Stack direction="row" spacing={2}>
+							<Button
+								// onClick={async () => {
+								// 	router.push(await generateAuthRequest("login"));
+								// }} //NOmoreLogin
+								variant="contained"
+								color="success"
+								style={{
+									backgroundColor: "#FFFFFF",
+									color: "black",
+									fontWeight: "bold",
+									fontSize: "1.2rem",
+									borderRadius: "2rem",
+									padding: "0.5rem 1rem",
+									margin: "0.5rem 1rem",
+									border: "none",
+									boxShadow: "0 0 0.5rem rgba(0, 0, 0, 0.5)",
+								}}
+							>
+
 								<Image src="/line.webp" alt="line" width={30} height={30} />
 
 								<span
 									style={{
 										marginRight: "1rem",
-										padding:"0.5rem"
+										padding: "0.5rem"
 									}}
 								>
 									Login with LINE
 								</span>
-							
-						</Button>
-					</Stack>
-				</Grid>
+
+							</Button>
+						</Stack>
+					</Grid>
+				</Paper>
 			</Container>
 		</div>
 	);
