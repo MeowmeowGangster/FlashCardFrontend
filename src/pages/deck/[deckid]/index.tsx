efd5964fb2d579236b7f7e666052bc1f32dd97c9
pattananNP <phattanun19@hotmail.com>
2022-11-12 20:36:58 +0700

    Create Deck

--- src/pages/deck/[deckid]/index.tsx
+++ src/pages/deck/[deckid]/index.tsx
@@ -3,181 +3,157 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getDeckById } from "@redux/actions/deck";
+import { deckById} from "@redux/selectors/decks.selector";
 
 const Deck: NextPage = () => {
 	const router = useRouter();
+	const dispatch = useDispatch<any>();
 	const { deckid } = router.query;
 
-	const mockData = [
-		{
-			name: "Card 1",
-			description: "This is the first card",
-			picture: "https://source.unsplash.com/random/200x200?sig=3",
-		},
-		{
-			name: "Card 2",
-			description: "This is the second card",
-			picture: "https://source.unsplash.com/random/200x200?sig=3",
-		},
-		{
-			name: "Card 3",
-			description: "This is the third card",
-			picture: "https://source.unsplash.com/random/200x200?sig=3",
-		},
-		{
-			name: "Card 4",
-			description: "This is the fourth card",
-			picture: "https://source.unsplash.com/random/200x200?sig=3",
-		},
+;
+	const deck = useSelector(deckById);
 
-		{
-			name: "Card 5",
-			description: "This is the fifth card",
-			picture: "https://source.unsplash.com/random/200x200?sig=3",
-		},
-		{
-			name: "Card 6",
-			description: "This is the sixth card",
-			picture: "https://source.unsplash.com/random/200x200?sig=3",
-		},
-		{
-			name: "Card 7",
-			description: "This is the seventh card",
-			picture: "https://source.unsplash.com/random/200x200?sig=3",
-		},
-		{
-			name: "Card 8",
-			description: "This is the eighth card",
-			picture: "https://source.unsplash.com/random/200x200?sig=3",
-		},
-	];
+	useEffect(() => {
+		console.log(deckid);
+		dispatch(getDeckById(deckid as string));
+	}, [deckid, dispatch]);
+
 	return (
 		<div className="bg">
-			<Stack
-				rowGap={5}
-				style={{
-					padding: "30px",
-				}}
-			>
-				<Container>
-					<Stack>
-						<Grid
-							container
-							style={{
-								alignItems: "start",
-							}}
-						>
-							<Grid item md={2}>
-								<p>{deckid}</p>
-							</Grid>
-							<Grid item md={2}>
-								<p>0</p>
-							</Grid>
-						</Grid>
-						<Container
-							style={{
-								width: "100%",
-								height: "650px",
-								overflowY: "scroll",
-							}}
-						>
+			<Container>
+				<Stack
+					rowGap={5}
+					style={{
+						padding: "10px",
+						height: "100vh",
+						overflowY: "scroll",
+						justifyContent: "center",
+						alignContent: "center",
+					}}
+				>
+					<Container>
+						<Stack>
 							<Grid
 								container
-								spacing={{ xs: 2, md: 3 }}
-								columns={{ xs: 4, sm: 8, md: 12 }}
-								rowGap={2}
+								style={{
+									alignItems: "start",
+								}}
 							>
-								{mockData.map((card, index) => (
-									<Grid item xs={2} sm={4} md={4} key={index}>
+								<Grid item md={2}>
+									<p>{deckid}</p>
+								</Grid>
+								<Grid item md={2}>
+									<p>0</p>
+								</Grid>
+							</Grid>
+							<Container
+								style={{
+									width: "100%",
+									height: "600px",
+									overflowY: "scroll",
+								}}
+							>
+								<Grid
+									container
+									spacing={{ xs: 1, md: 3 }}
+									columns={{ xs: 4, sm: 8, md: 12 }}
+									rowGap={2}
+								>
+									{deck?.cards?.map((card, index) => (
+										<Grid item xs={2} sm={4} md={4} key={index}>
+											<Button
+												style={{
+													backgroundColor: "white",
+													borderRadius: "10px",
+													color: "black",
+													padding: "20px",
+													fontSize: "12px",
+													width: "120px",
+													height: "180px",
+													textAlign: "center",
+													verticalAlign: "middle",
+													textDecoration: "none",
+													boxShadow: "#000",
+												}}
+											>
+												<Box>
+													<h2>{card.name}</h2>
+													<p>{card.description}</p>
+												</Box>
+											</Button>
+										</Grid>
+									))}
+									<Grid item xs={2} sm={4} md={4}>
 										<Button
 											style={{
-												backgroundColor: "white",
+												backgroundColor: "#fb923c",
 												borderRadius: "10px",
-												color: "black",
-												padding: "20px",
-												fontSize: "12px",
+												color: "#ffffff",
+												padding: "32px",
+												fontSize: "100px",
 												width: "120px",
-												height: "120px",
+												height: "180px",
 												textAlign: "center",
 												verticalAlign: "middle",
 												textDecoration: "none",
 												boxShadow: "#000",
 											}}
 										>
-											<Box>
-												<h2>{card.name}</h2>
-												<p>{card.description}</p>
-											</Box>
+											<AddCircleIcon />
 										</Button>
 									</Grid>
-								))}
-								<Grid item xs={2} sm={4} md={4}>
-									<Button
-										style={{
-											backgroundColor: "#fb923c",
-											borderRadius: "10px",
-											color: "#ffffff",
-											padding: "32px",
-											fontSize: "100px",
-											width: "120px",
-											height: "120px",
-											textAlign: "center",
-											verticalAlign: "middle",
-											textDecoration: "none",
-											boxShadow: "#000",
-										}}
-									>
-										<AddCircleIcon />
-									</Button>
 								</Grid>
+							</Container>
+						</Stack>
+					</Container>
+					<Stack>
+						<Grid
+							container
+							spacing={12}
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								alignItems: "center",
+							}}
+						>
+							<Grid item xs={6}>
+								<Button
+									onClick={() => {
+										router.back();
+									}}
+									style={{
+										backgroundColor: "#8A9098",
+										color: "white",
+										borderRadius: "50px",
+										padding: "10px 20px",
+										fontFamily: "Prompt",
+										width: "120px",
+									}}
+								>
+									Cancel
+								</Button>
 							</Grid>
-						</Container>
+							<Grid item xs={6}>
+								<Button
+									style={{
+										backgroundColor: "#FDE68A",
+										color: "black",
+										borderRadius: "50px",
+										padding: "10px 20px",
+										fontFamily: "Prompt",
+										width: "120px",
+									}}
+								>
+									Play
+								</Button>
+							</Grid>
+						</Grid>
 					</Stack>
-				</Container>
-				<Stack>
-					<Grid
-						container
-						spacing={12}
-						style={{
-							display: "flex",
-							flexDirection: "row",
-							alignItems: "center",
-						}}
-					>
-						<Grid item xs={6}>
-							<Button
-								onClick={() => {
-									router.back();
-								}}
-								style={{
-									backgroundColor: "#8A9098",
-									color: "white",
-									borderRadius: "50px",
-									padding: "10px 20px",
-									fontFamily: "Prompt",
-									width: "120px",
-								}}
-							>
-								Cancel
-							</Button>
-						</Grid>
-						<Grid item xs={6}>
-							<Button
-								style={{
-									backgroundColor: "#FDE68A",
-									color: "black",
-									borderRadius: "50px",
-									padding: "10px 20px",
-									fontFamily: "Prompt",
-									width: "120px",
-								}}
-							>
-								Play
-							</Button>
-						</Grid>
-					</Grid>
 				</Stack>
-			</Stack>
+			</Container>
 		</div>
 	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,76 +6,101 @@ import { useEffect, useState } from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { useSelector } from "react-redux";
 import { decksData } from "@redux/selectors/decks.selector";
-import { getDecks } from "@redux/actions/deck";
+import { getDecks, createDeck } from "@redux/actions/deck";
 import { useDispatch } from "react-redux";
-import { useRouter } from "next/router";
 import Link from "next/link";
+import { CreateDeck } from "@interfaces/decks.interface";
 
 const Home: NextPage = () => {
 	const [open, setOpen] = useState(false);
 	const [deckname, setDeckName] = useState("");
-	const router = useRouter();
+	const [deckStatus, setDeckStatus] = useState("idle");
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setDeckName(event.target.value);
 	};
 	const dispatch = useDispatch<any>();
 	const decks = useSelector(decksData);
 
+	const CreateNewDeck = () => {
+		const data: CreateDeck = {
+			deckName: deckname,
+		};
+		try {
+			dispatch(createDeck(data));
+		} catch (e) {
+			console.log(e);
+		} finally {
+			setDeckStatus("idle");
+		}
+		setOpen(false);
+		setDeckName("");
+	};
+
 	useEffect(() => {
-		dispatch(getDecks());
-	}, [dispatch]);
+		if (deckStatus === "idle") {
+			dispatch(getDecks());
+		}
+	}, [deckStatus, dispatch]);
+
+	console.log(deckStatus);
 
 	return (
 		<div className="bg">
 			<PopModal isOpen={open} setIsOpen={setOpen}>
-				<Stack rowGap={4}>
-					<Typography id="modal-modal-title" variant="h6" component="h2">
-						Your Deck Name
-					</Typography>
+				<form onSubmit={CreateNewDeck}>
+					<Stack rowGap={4}>
+						<Typography id="modal-modal-title" variant="h6" component="h2">
+							Your Deck Name
+						</Typography>
 
-					<TextField
-						value={deckname}
-						variant="outlined"
-						onChange={handleChange}
-						placeholder="Deck Name"
-						style={{
-							backgroundColor: "white",
-							borderRadius: "50px",
-							height: "50px",
-						}}
-					/>
-					<Grid container spacing={12}>
-						<Grid item xs={6}>
-							<Button
-								onClick={() => setOpen(false)}
-								style={{
-									backgroundColor: "#8A9098",
-									color: "white",
-									borderRadius: "50px",
-									padding: "10px 20px",
-									fontFamily: "Prompt",
-									width: "100px",
-								}}
-							>
-								Cancel
-							</Button>
+						<TextField
+							value={deckname}
+							variant="outlined"
+							onChange={handleChange}
+							placeholder="Deck Name"
+							style={{
+								backgroundColor: "white",
+								borderRadius: "50px",
+								height: "50px",
+							}}
+						/>
+						<Grid container spacing={12}>
+							<Grid item xs={6}>
+								<Button
+									onClick={() => setOpen(false)}
+									style={{
+										backgroundColor: "#8A9098",
+										color: "white",
+										borderRadius: "50px",
+										padding: "10px 20px",
+										fontFamily: "Prompt",
+										width: "100px",
+									}}
+								>
+									Cancel
+								</Button>
+							</Grid>
+							<Grid item xs={6}>
+								<Button
+									type="submit"
+									onClick={() => {
+										setDeckStatus("pending");
+									}}
+									style={{
+										backgroundColor: "#FDE68A",
+										color: "black",
+										borderRadius: "50px",
+										padding: "10px 20px",
+										fontFamily: "Prompt",
+										width: "100px",
+									}}
+								>
+									Go
+								</Button>
+							</Grid>
 						</Grid>
-						<Grid item xs={6}>
-							<Button
-								style={{
-									backgroundColor: "#FDE68A",
-									color: "black",
-									borderRadius: "50px",
-									padding: "10px 20px",
-									fontFamily: "Prompt",
-									width: "100px",
-								}}
-							>
-								Go
-							</Button>
-						</Grid>
-					</Grid>
-				</Stack>
+					</Stack>
+				</form>
 			</PopModal>
 			<Container>
 				<Stack
@@ -86,7 +111,6 @@ const Home: NextPage = () => {
 						overflowY: "scroll",
 					}}
 				>
-					
 					<h1>Choose Your Deck</h1>
 
 					<Grid
@@ -103,7 +127,7 @@ const Home: NextPage = () => {
 						{decks.length > 0 &&
 							decks.map((deck: any, idx: number) => (
 								<Grid item xs={12} key={idx}>
-									<Link href={`/deck/${deck._id}`} passHref>
+									<Link href={`/deck/${deck.deckID}`} passHref>
 										<Button
 											style={{
 												backgroundColor: "#ffffff",
@@ -130,7 +154,14 @@ const Home: NextPage = () => {
 													{deck.deckName}
 												</Typography>
 
-												<p>ID:&nbsp;{deck._id}</p>
+												<p
+												style={{
+													borderRadius: "40px",
+													padding: "20px 20px",
+													fontSize: "10px",
+													fontWeight: "bold",
+												}}
+												>ID:&nbsp;{deck.deckID}</p>
 											</Stack>
 										</Button>
 									</Link>
